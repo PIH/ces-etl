2b47988aa2ddfae0a6e47756fff213817afac676
Brandon Istenes <bistenes@gmail.com>
2021-09-20 13:38:02 -0700

    Update source-db.sh

--- source-db.sh
+++ source-db.sh
@@ -13,7 +13,7 @@ function db_name {
 dropDB() {
         for site in ${SITES[@]}
         do
-                db=$(db_name ${site}})
+                db=$(db_name ${site})
                 echo "Dropping of ${db} started at ${TIME}"  >> ${LOGS}/dbdrop_${DATE}.log
                 mysql -h 127.0.0.1 -u${ROOT} -p${ROOTPWD} -e "drop database if exists ${db};"
                 echo "Dropping of ${db} ended at ${TIME}"  >> ${LOGS}/dbdrop_${DATE}.log
@@ -23,7 +23,7 @@ dropDB() {
 createDB() {
         for site in ${SITES[@]}
         do
-                db=$(db_name ${site}})
+                db=$(db_name ${site})
                 echo "Creation of ${db} started at ${TIME}"  >> ${LOGS}/dbcreate_${DATE}.log
                 mysql -h 127.0.0.1 -u${ROOT} -p${ROOTPWD} -e "create database ${db} default char set utf8;"
                 echo "Creation of ${db} ended at ${TIME}"  >> ${LOGS}/dbcreate_${DATE}.log
@@ -34,7 +34,7 @@ sourceDB() {
 
         for site in ${SITE[@]}
         do
-                db=$(db_name ${site}})
+                db=$(db_name ${site})
                 echo "Import of ${db} started at ${TIME}"  >> ${LOGS}/dbrestore_${DATE}.log
                 mysql -h 127.0.0.1 -u${ROOT} -p${ROOTPWD} ${db} < ${DESTINATION}/openmrs-${site}/${DATABASE}
                 echo "Import of ${db} ended at ${TIME}"  >> ${LOGS}/dbrestore_${DATE}.log
