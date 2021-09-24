28cbe36c45b9ef6eb170e2e4ab4c504ded7cc5b9
Brandon Istenes <bistenes@gmail.com>
2021-09-23 17:07:11 -0700

    Update pull-db.sh

--- pull-db.sh
+++ pull-db.sh
@@ -5,37 +5,35 @@ set +x
 source common.sh
 source .env
 
-DESTINATION=./tmp/
-
 # delete old data dumps
 removeDirs() {
-        rm -rf ${DESTINATION}
+        rm -rf ${TMP}
 }
 
 # create destination dirs
 createDirs() {
-        mkdir -p ${DESTINATION}
+        mkdir -p ${TMP}
         mkdir -p ${LOGS}
 }
 
 # down the database from the blobs
 downloadDatabases() {
         for item in ${SITES[@]}
-        do
+        do  
                 url=${DOWNLOAD_URL1}/${item}/sequences/${item}_${DOWNLOAD_URL2}
                 echo "Downloading backup for ${item}: ${url}"
-                azcopy copy "${url}" "${DESTINATION}" --overwrite=prompt --check-md5 FailIfDifferent --from-to=BlobLocal --blob-type Detect --recursive;
+                azcopy copy "${url}" "${TMP}" --overwrite=prompt --check-md5 FailIfDifferent --from-        to=BlobLocal --blob-type Detect --recursive;
         done
-        }
+        }   
 
 # extract the databases
 extractDb() {
         for item in ${SITES[@]}
-        do
-                target=${DESTINATION}/openmrs-${item}/openmrs.sql
+        do  
+                target=${TMP}/openmrs-${item}/openmrs.sql
                 echo "Extracting to ${target}"
-                7za e -p"${PASS}" ${DESTINATION}/${item}_backup_${DATE}.sql.7z -o${DESTINATION}/openmrs-${item}
-                mv ${DESTINATION}/openmrs-${item}/y ${target}
+                7za e -p"${PASS}" ${TMP}/${item}_backup_${DATE}.sql.7z -o${TMP}/openmrs-${item}
+                mv ${TMP}/openmrs-${item}/y ${target}
         done
 }
 
