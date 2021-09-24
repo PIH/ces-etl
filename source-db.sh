2a780bd4e7eccb7752adba1878dea5a0abb887a3
Brandon Istenes <bistenes@gmail.com>
2021-09-23 17:07:57 -0700

    Update source-db.sh

--- source-db.sh
+++ source-db.sh
@@ -1,12 +1,12 @@
 #!/bin/bash
   
+source common.sh
 source .env
+
 DATABASE="openmrs.sql"
 
-TIME=`date +%Y%m%d-%H%M%S`
-
 function db_name {
-        return "openmrs_$(sed 's/-/_/g' <<< $1)"
+        echo "openmrs_$(sed 's/-/_/g' <<< $1)"
 }
 
 
@@ -14,9 +14,9 @@ dropDB() {
         for site in ${SITES[@]}
         do
                 db=$(db_name ${site})
-                echo "Dropping of ${db} started at ${TIME}"  >> ${LOGS}/dbdrop_${DATE}.log
+                echo "Dropping of ${db} started at $(date)"  | tee -a ${LOGS}/dbdrop_${DATE}.log
                 mysql -h 127.0.0.1 -u${ROOT} -p${ROOTPWD} -e "drop database if exists ${db};"
-                echo "Dropping of ${db} ended at ${TIME}"  >> ${LOGS}/dbdrop_${DATE}.log
+                echo "Dropping of ${db} ended at $(date)"  | tee -a ${LOGS}/dbdrop_${DATE}.log
         done
 }
 
@@ -24,21 +24,22 @@ createDB() {
         for site in ${SITES[@]}
         do
                 db=$(db_name ${site})
-                echo "Creation of ${db} started at ${TIME}"  >> ${LOGS}/dbcreate_${DATE}.log
+                echo "Creation of ${db} started at $(date)"  | tee -a ${LOGS}/dbcreate_${DATE}.log
                 mysql -h 127.0.0.1 -u${ROOT} -p${ROOTPWD} -e "create database ${db} default char set utf8;"
-                echo "Creation of ${db} ended at ${TIME}"  >> ${LOGS}/dbcreate_${DATE}.log
+                echo "Creation of ${db} ended at $(date)"  | tee -a ${LOGS}/dbcreate_${DATE}.log
         done
 }
 
 sourceDB() {
 
-        for site in ${SITE[@]}
-        do
+        for site in ${SITES[@]}
+        do  
                 db=$(db_name ${site})
-                echo "Import of ${db} started at ${TIME}"  >> ${LOGS}/dbrestore_${DATE}.log
-                mysql -h 127.0.0.1 -u${ROOT} -p${ROOTPWD} ${db} < ${DESTINATION}/openmrs-${site}/${DATABASE}
-                echo "Import of ${db} ended at ${TIME}"  >> ${LOGS}/dbrestore_${DATE}.log
-        done
+                echo "Import of ${db} started at $(date)"  | tee -a ${LOGS}/dbrestore_${DATE}.log
+                mysql -h 127.0.0.1 -u${ROOT} -p${ROOTPWD} ${db} < ${TMP}/openmrs-${site}/${DATABASE} && \
+            curl -fsS -m 10 --retry 5 -o /dev/null ${HEALTHCHECK_FOR_SITE["$site"]}
+                echo "Import of ${db} ended at $(date)"  | tee -a ${LOGS}/dbrestore_${DATE}.log
+        done    
 }
 
 
