19a7e1bf81161767a048ec6d849c3d32d17786bb
Brandon Istenes <bistenes@gmail.com>
2021-09-27 14:01:43 -0700

    Update source-db.sh

--- source-db.sh
+++ source-db.sh
@@ -1,5 +1,9 @@
 #!/bin/bash
-  
+#
+# Requires the following variables in `.env`:
+#   - DBUSER
+#   - DBPASS
+
 source common.sh
 source .env
 
@@ -11,13 +15,13 @@ do
     if [[ -f ${backupFile} ]]; then
         db=openmrs_$(sed 's/-/_/g' <<< ${site})
         echo "Dropping ${db} at $(date)"  | tee -a ${LOGS}/dbdrop_${DATE}.log
-        mysql -h 127.0.0.1 -u${ROOT} -p${ROOTPWD} -e "drop database if exists ${db};"
+        mysql -h 127.0.0.1 -u${DBUSER} -p${DBPASS} -e "drop database if exists ${db};"
         sleep 10
         echo "Creating ${db} at $(date)"  | tee -a ${LOGS}/dbcreate_${DATE}.log
-        mysql -h 127.0.0.1 -u${ROOT} -p${ROOTPWD} -e "create database ${db} default char set utf8;"
+        mysql -h 127.0.0.1 -u${DBUSER} -p${DBPASS} -e "create database ${db} default char set utf8;"
         sleep 10
         echo "Import of ${db} started at $(date)"  | tee -a ${LOGS}/dbrestore_${DATE}.log
-        mysql -h 127.0.0.1 -u${ROOT} -p${ROOTPWD} ${db} < ${backupFile} && \
+        mysql -h 127.0.0.1 -u${DBUSER} -p${DBPASS} ${db} < ${backupFile} && \
             curl -fsS -m 10 --retry 5 -o /dev/null ${HEALTHCHECK_FOR_SITE["$site"]}
         echo "Import of ${db} ended at $(date)"  | tee -a ${LOGS}/dbrestore_${DATE}.log
     else
