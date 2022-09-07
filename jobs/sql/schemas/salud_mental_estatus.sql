fa99b72cccd593ac354284c91c415327d52440c1
aibrahem550 <ahmed.ibrahem550@gmail.com>
2022-09-07 15:42:59 +0400

    adding mental estatus v1

---
+++ jobs/sql/schemas/salud_mental_estatus.sql
@@ -0,0 +1,7 @@
+CREATE TABLE salud_mental_estatus (
+dbname varchar(30),
+patient_id int,
+emr_id varchar(30),
+emr_instancia varchar(30),
+resultado_salud_mental varchar(30),
+resultado_salud_mental_fecha date);
