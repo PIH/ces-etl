545b24906e7145c81c36f6d63f58d0d0e8289630
James Mbabazi <jmbabazi@pih.org>
2022-09-08 12:04:43 -0400

    changing dtype for resultado_salud_mental

--- jobs/sql/extractions/salud_mental_estatus.sql
+++ jobs/sql/extractions/salud_mental_estatus.sql
@@ -7,7 +7,7 @@ dbname varchar(30),
 patient_id int,
 emr_id varchar(30),
 emr_instancia varchar(30),
-resultado_salud_mental varchar(30),
+resultado_salud_mental text,
 resultado_salud_mental_fecha date);
 
 CREATE OR REPLACE VIEW patient_identifier_v2 AS
--- jobs/sql/schemas/salud_mental_estatus.sql
+++ jobs/sql/schemas/salud_mental_estatus.sql
@@ -3,5 +3,5 @@ dbname varchar(30),
 patient_id int,
 emr_id varchar(30),
 emr_instancia varchar(30),
-resultado_salud_mental varchar(30),
+resultado_salud_mental text,
 resultado_salud_mental_fecha date);
