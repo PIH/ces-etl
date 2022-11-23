3fa45923caef54b93dea9e36c4c94584956f8463
aibrahem550 <ahmed.ibrahem550@gmail.com>
2022-11-23 15:23:08 +0200

    commit

--- jobs/sql/schemas/programas.sql
+++ jobs/sql/schemas/programas.sql
@@ -2,7 +2,7 @@ create table programas(
 emrid varchar(50),
 emr_instance varchar(50),
 program varchar(50),
-hearts boolean,
+hearts bit,
 hearts_date date,
 date_enrolled datetime,
 date_unenroll datetime,
