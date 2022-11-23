08678885a19dc8efb89bfd5cde8e41ecd1933602
dmdesimone <ddesimone@pih.org>
2022-11-23 12:10:35 -0500

    translate columns into Spanish

--- jobs/sql/schemas/programas.sql
+++ jobs/sql/schemas/programas.sql
@@ -1,12 +1,12 @@
-create table programas(
-emr_id varchar(50),
-emr_instance varchar(50),
-program varchar(50),
-hearts bit,
-hearts_date date,
-date_enrolled datetime,
-date_unenroll datetime,
-program_status varchar(50),
-index_asc int,
-index_desc int
+create table programas (
+  emr_id            varchar(50),
+  emr_instancia     varchar(50),
+  programa          varchar(50),
+  hearts            bit,
+  hearts_fecha      date,
+  fecha_inscrito    datetime,
+  fecha_salida      datetime,
+  estatus           varchar(50),
+  index_asc         int,
+  index_desc        int
 );
