4d9cb9c3460a2f7184e9fc0e0ae3348fa1a6db0e
James Mbabazi <12864648+jmbabazi@users.noreply.github.com>
2022-12-13 15:34:56 +0200

    update users columns to spanish

--- jobs/sql/schemas/users.sql
+++ jobs/sql/schemas/users.sql
@@ -1,14 +1,14 @@
 CREATE TABLE users
 (
-    username            varchar(50),
-    first_name          varchar(50),
-    last_name           varchar(50),
-    account_enabled     bit,
-    created_date        datetime,
-    created_by          varchar(50),
-    provider_type       varchar(50),
-    last_login_date     datetime,
-    num_logins_recorded int
+    usuario             varchar(50),
+    nombres             varchar(50),
+    apellidos           varchar(50),
+    cuenta_activada     bit,
+    fecha_creado        datetime,
+    creado_por          varchar(50),
+    tipo_proveedor      varchar(50),
+    fecha_última_acceso     datetime,
+    número_accesos_registrados int
 )
 ;
 
