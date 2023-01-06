6f48aa5c3008d7d8a823d8dba201aaaf8da467a2
dmdesimone <ddesimone@pih.org>
2023-01-06 16:14:01 -0500

    MEX-546 add uuids

--- jobs/sql/schemas/programas.sql
+++ jobs/sql/schemas/programas.sql
@@ -1,12 +1,14 @@
 create table programas (
-  emr_id            varchar(50),
-  emr_instancia     varchar(50),
-  programa          varchar(50),
-  hearts            bit,
-  hearts_fecha      date,
-  fecha_inscrito    date,
-  fecha_salida      date,
-  estatus           varchar(50),
-  index_asc         int,
-  index_desc        int
+  emr_id               varchar(50),
+  person_uuid          char(38),
+  patient_program_uuid char(38),
+  emr_instancia        varchar(50),
+  programa             varchar(50),
+  hearts               bit,
+  hearts_fecha         date,
+  fecha_inscrito       date,
+  fecha_salida         date,
+  estatus              varchar(50),
+  index_asc            int,
+  index_desc           int
 );
