314a594de4c62116143ae9acdfedd96cb1809124
dmdesimone <ddesimone@pih.org>
2023-01-14 23:09:49 -0500

    mex-555 add encuentro_consulta

--- jobs/sql/derivations/deduplicate_tables.sql
+++ jobs/sql/derivations/deduplicate_tables.sql
@@ -1,7 +1,7 @@
 --
 -- ces_pacientes
 --
--- update uuids that should be changes because of merging
+-- update uuids that should be changed because of merging
 drop table if exists #staging_pacientes;
 select * into #staging_pacientes from ces_pacientes_staging cp; 
 
@@ -20,6 +20,45 @@ where emr_id =
 	where sp2.person_uuid = sp.person_uuid 
 	order by ultimo_encuentro_fecha desc,
 	iif(case sp2.registracion_localidad
+		when 'Soledad' then 'soledad'
+		when 'Salvador' then 'salvador'
+		when 'Monterrey' then 'monterrey'
+		when 'Matazano' then 'matazano'
+		when 'Letrero' then 'letrero'
+		when 'Laguna del Cofre' then 'laguna'
+		when 'Capitan' then 'capitan'
+		when 'Honduras' then 'honduras'
+		when 'Casa Materna' then 'jaltenango'
+		when 'CER' then 'jaltenango'
+		when 'CES Oficina' then 'jaltenango'
+		when 'Hospital' then 'jaltenango'	
+		when 'Pediatría' then 'jaltenango'
+		when 'Reforma' then 'jaltenango'
+		when 'CER' then 'jaltenango'
+		when 'CES Oficina' then 'jaltenango'
+		when 'Hospital' then 'jaltenango'		
+	   end = site, 1, 0) desc)
+;
+--
+-- encuentro_consulta
+--
+-- update uuids that should be changed because of merging
+drop table if exists #staging_encuentro_consulta;
+select * into #staging_encuentro_consulta from encuentro_consulta_staging; 
+
+update sec
+set sec.person_uuid = mh.winner_person_uuid
+from #staging_encuentro_consulta sec
+inner join merge_history mh on mh.loser_person_uuid = sec.person_uuid 
+; 
+
+-- choose single encounter row based on rows where encounter location = site 
+drop table if exists encuentro_consulta;
+select * into encuentro_consulta from #staging_encuentro_consulta sec
+where sec.encuentro_id  =
+	(select top 1 sec2.encuentro_id from #staging_encuentro_consulta sec2
+	where sec2.encounter_uuid = sec.encounter_uuid 
+	order by iif(case sec2.emr_instancia
 		when 'Soledad' then 'soledad'
 		when 'Salvador' then 'salvador'
 		when 'Monterrey' then 'monterrey'
