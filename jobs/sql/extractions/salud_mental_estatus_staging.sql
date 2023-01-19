e0d1fe4fdab85b4c86ed9320dd0603438c4c1eca
aibrahem550 <ahmed.ibrahem550@gmail.com>
2023-01-19 16:31:16 +0200

    MEX_557

--- jobs/sql/extractions/salud_mental_estatus_staging.sql
+++ jobs/sql/extractions/salud_mental_estatus_staging.sql
@@ -1,3 +1,5 @@
+SET @partition = '${partitionNum}';
+
 SELECT program_id INTO @program_id FROM program p WHERE uuid='0e69c3ab-1ccb-430b-b0db-b9760319230f';
 select patient_identifier_type_id into @identifier_type from patient_identifier_type pit where uuid ='506add39-794f-11e8-9bcd-74e5f916c5ec';
 select encounter_type_id into @reg_encounter from encounter_type et where uuid='873f968a-73a8-4f9c-ac78-9f4778b751b6';
@@ -9,13 +11,15 @@ SELECT concept_id INTO @transfered FROM concept WHERE uuid='3cdd5c02-26fe-102b-8
 SELECT concept_id INTO @patient_refused FROM concept WHERE uuid='efab937b-853e-47da-b97e-220f1bdff97d';
 SELECT concept_id INTO @completed FROM concept WHERE uuid='3cdcecea-26fe-102b-80cb-0017a47871b2';
 
-set @dbname = '${partitionNum}';
+SELECT @program_id
 
 DROP TABLE IF EXISTS salud_mental_estatus;
 CREATE TEMPORARY TABLE salud_mental_estatus (
-dbname varchar(30),
 patient_id int,
+person_uuid char(38),
 emr_id varchar(30),
+patient_program_uuid char(38),
+date_changed date,
 emr_instancia varchar(30),
 resultado_salud_mental text,
 int_rank int,
@@ -33,9 +37,11 @@ GROUP BY patient_id;
 
 truncate table salud_mental_estatus;
 
-insert into salud_mental_estatus (patient_id, emr_id, emr_instancia, resultado_salud_mental, int_rank,resultado_salud_mental_fecha)
+
+insert into salud_mental_estatus (patient_id, emr_id, emr_instancia, resultado_salud_mental, int_rank,resultado_salud_mental_fecha, patient_program_uuid, date_changed)
 SELECT distinct pp.patient_id,pi2.identifier 'emr_id', l.name 'emr_instancia',
-	  'inscrito' as 'resultado_salud_mental' , 1 as int_rank, date_enrolled  'resultado_salud_mental_fecha'
+	  'inscrito' as 'resultado_salud_mental' , 1 as int_rank, date_enrolled  'resultado_salud_mental_fecha',
+	  pp.uuid AS patient_program_uuid, pp.date_changed
 FROM patient_program pp 
 inner join tmp_patient_identifier_v2 pi2 on pi2.patient_id = pp.patient_id  
 left outer join location l on pp.location_id =l.location_id 
@@ -48,7 +54,8 @@ SELECT DISTINCT pp.patient_id,pi2.identifier 'emr_id', l.name 'emr_instancia',
 	  when cn.concept_id =@patient_refused then 4
 	  when cn.concept_id =@completed then 5
 	  when cn.concept_id =@muerte then 6 end as int_rank,
-	  date_completed  'resultado_salud_mental_fecha'
+	  date_completed  'resultado_salud_mental_fecha',
+	  pp.uuid AS patient_program_uuid, pp.date_changed
 FROM patient_program pp 
 inner join tmp_patient_identifier_v2 pi2 on pi2.patient_id = pp.patient_id  
 left outer join location l on pp.location_id =l.location_id 
@@ -58,9 +65,6 @@ and cn.locale ='en'
 and cn.voided =0 and cn.concept_name_type='FULLY_SPECIFIED'
 ;
 
-
-update salud_mental_estatus t
-set t.dbname=@dbname;
 
 update salud_mental_estatus t 
 set t.emr_instancia = (
@@ -77,7 +81,7 @@ where t.emr_instancia is null;
 -- ---- Ascending Order ------------------------------------------
 drop table if exists int_asc;
 create table int_asc
-select * from salud_mental_estatus
+select DISTINCT s.* from salud_mental_estatus s
 ORDER BY patient_id asc, resultado_salud_mental_fecha asc, int_rank asc;
 
 
@@ -92,17 +96,23 @@ SELECT
         ELSE 1
     END AS index_asc,
     @patient_id:=patient_id patient_id,
-    resultado_salud_mental_fecha,int_rank
+    resultado_salud_mental_fecha,int_rank, patient_program_uuid
 FROM
     int_asc;
-    
+   
+--    SELECT * FROM asc_order WHERE patient_id =39
+--     GROUP BY patient_id,resultado_salud_mental_fecha,int_rank
+--  HAVING count(*) > 1
+--  
+   
 update salud_mental_estatus es
 set es.index_asc = (
- select index_asc 
+ select DISTINCT index_asc
  from asc_order
- where patient_id=es.patient_id 
+ where patient_id= es.patient_id 
  and resultado_salud_mental_fecha=es.resultado_salud_mental_fecha
  and int_rank=es.int_rank
+ AND patient_program_uuid=es.patient_program_uuid
 );
 
 -- -------- Descending Order --------------------------------------------------    
@@ -120,7 +130,7 @@ SELECT
         ELSE 1
     END AS index_desc,
     @patient_id:=patient_id patient_id,
-    resultado_salud_mental_fecha,int_rank
+    resultado_salud_mental_fecha,int_rank, patient_program_uuid
 FROM
     int_desc;
     
@@ -131,13 +141,28 @@ set es.index_desc = (
  where patient_id=es.patient_id 
  and resultado_salud_mental_fecha=es.resultado_salud_mental_fecha
  and int_rank=es.int_rank
+ AND patient_program_uuid=es.patient_program_uuid
 );
+
+-- -----------------------------------------
+
+UPDATE salud_mental_estatus sm 
+INNER JOIN (
+	SELECT DISTINCT p2.person_id, 
+	uuid
+	FROM person p2
+) x ON sm.patient_id =x.person_id
+SET sm.person_uuid = x.uuid;
+
 
 -- -----------------------------------------
 
 select 
 distinct 
-emr_id,
+CONCAT(@partition,'-',emr_id) "emr_id" ,
+person_uuid,
+patient_program_uuid,
+date_changed,
 emr_instancia,
 resultado_salud_mental,
 resultado_salud_mental_fecha,
--- jobs/sql/schemas/salud_mental_estatus.sql
+++
@@ -1,7 +0,0 @@
-CREATE TABLE salud_mental_estatus (
-emr_id varchar(30),
-emr_instancia varchar(30),
-resultado_salud_mental text,
-resultado_salud_mental_fecha datetime,
-index_asc int,
-index_desc int);
