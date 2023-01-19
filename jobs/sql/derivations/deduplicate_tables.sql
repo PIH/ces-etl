fc7089a71bb95147124006c6314175e954a4573f
dmdesimone <ddesimone@pih.org>
2023-01-19 13:04:21 -0500

    mex-555 add salud_mental_pacientes

--- jobs/sql/derivations/deduplicate_tables.sql
+++ jobs/sql/derivations/deduplicate_tables.sql
@@ -203,3 +203,44 @@ where s.diagnosticos_id  =
 		when 'Hospital' then 'jaltenango'		
 	   end = site, 1, 0) desc)
 ;
+--
+-- salud_mental_paciente
+--
+-- update uuids that should be changed because of merging
+drop table if exists #staging_salud_mental_paciente;
+select * into #staging_salud_mental_paciente from salud_mental_paciente_staging cp; 
+
+update sp
+set sp.person_uuid = mh.winner_person_uuid
+from #staging_salud_mental_paciente sp 
+inner join merge_history mh on mh.loser_person_uuid = sp.person_uuid 
+; 
+
+-- choose single pacientes row based on row with latest encounter
+-- ordered by last updated, and then prioritizing where the patient registration equals the site 
+drop table if exists salud_mental_paciente;
+select * into salud_mental_paciente from #staging_salud_mental_paciente sp
+where emr_id =
+	(select top 1 emr_id from  #staging_salud_mental_paciente sp2
+	where sp2.person_uuid = sp.person_uuid 
+	order by ultimo_visita_salud_mental_fecha desc,
+	iif(case sp2.inscripcion_salud_mental_localidad
+		when 'Soledad' then 'soledad'
+		when 'Salvador' then 'salvador'
+		when 'Monterrey' then 'monterrey'
+		when 'Matazano' then 'matazano'
+		when 'Letrero' then 'letrero'
+		when 'Laguna del Cofre' then 'laguna'
+		when 'Capitan' then 'capitan'
+		when 'Honduras' then 'honduras'
+		when 'Casa Materna' then 'jaltenango'
+		when 'CER' then 'jaltenango'
+		when 'CES Oficina' then 'jaltenango'
+		when 'Hospital' then 'jaltenango'	
+		when 'Pediatría' then 'jaltenango'
+		when 'Reforma' then 'jaltenango'
+		when 'CER' then 'jaltenango'
+		when 'CES Oficina' then 'jaltenango'
+		when 'Hospital' then 'jaltenango'		
+	   end = site, 1, 0) desc)
+;
